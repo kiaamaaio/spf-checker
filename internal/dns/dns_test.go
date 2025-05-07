f4e4064595a4e75c8c23acf359cb64d7c558d307
kiaamaaio <kiaamaaio@gmail.com>
2025-05-08 00:09:11 +0900

    spfの文法が誤っている場合のテストケースを追加

--- internal/dns/dns_test.go
+++ internal/dns/dns_test.go
@@ -1,6 +1,9 @@
 package dns
 
-import "testing"
+import (
+	"errors"
+	"testing"
+)
 
 func TestGetSpfRecord_Valid(t *testing.T) {
 	testTxtRecord := "v=spf1 ip4:192.0.2.1/24 -all"
@@ -18,4 +21,27 @@ func TestGetSpfRecord_Valid(t *testing.T) {
 	if record != testTxtRecord {
 		t.Errorf("unexpected record: %v", record)
 	}
+}
+
+func TestGetSpfRecord_Invalid(t *testing.T) {
+	testTxtRecord := "v=SPF1 ip4:192.0.2.1/24 -all"
+	txtLookupFunc = func(name string) ([]string, error) {
+		return []string{"example-domain-verification=test1test1test1test1test1test1", testTxtRecord}, nil
+	}
+	defer func() { txtLookupFunc = nil }()
+
+	d := NewDomain("example.test")
+	record, err := d.GetSpfRecord()
+
+	if errors.Is(err, ErrorNoSpfRecord) {
+		return
+	}
+	if err != nil {
+		t.Fatalf("unexpected error: %v", err)
+	}
+	if record != testTxtRecord {
+		t.Errorf("unexpected record: %v", record)
+	}
+
+	t.Fatal("unexpected error")
 }
